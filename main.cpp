27e87336565553829bfe7411e870bcbf2eb05355
2354970 <143269060+2354970@users.noreply.github.com>
2023-09-08 15:31:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+    int i=2, n=0, primo=1, secondo=1, terzo;
+    cin>>n;
+    if (n>=2){
+        cout<< primo << endl;
+        cout<< secondo << endl;
+        for(;i<n;i++){
+            terzo=primo+secondo;
+            primo=secondo;
+            secondo=terzo;
+            cout<< terzo << endl;
+        }
+    } else {
+        cout << "errore" << endl;
+    }
    return 0;
 }
